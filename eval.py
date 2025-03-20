86811f1866980c7f458193dcabe052b49c924ec5
qiulingyan <kiwiqiuqiu@163.com>
2025-03-20 16:31:56 +0800

    eval.py:自蒸馏模板，有中间层acc

--- eval.py
+++ eval.py
@@ -7,10 +7,10 @@ from macls.utils.utils import add_arguments, print_arguments
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
-add_arg('configs',          str,   'configs/panns.yml',         "配置文件") #改
+add_arg('configs',          str,   'configs/resnet_se.yml',         "配置文件") #改
 add_arg("use_gpu",          bool,  True,                        "是否使用GPU评估模型")
 add_arg('save_matrix_path', str,   'output/images/',            "保存混合矩阵的路径")
-add_arg('resume_model',     str,   'models/PANNS_CNN6_MelSpectrogram/best_model/',  "模型的路径") #改
+add_arg('resume_model',     str,   'models/ResNetSE_MelSpectrogram/best_model/',  "模型的路径") #改
 args = parser.parse_args()
 print_arguments(args=args)
 
@@ -19,11 +19,11 @@ trainer = MAClsTrainer(configs=args.configs, use_gpu=args.use_gpu)
 
 # 开始评估
 start = time.time()
-loss, accuracy = trainer.evaluate(resume_model=args.resume_model,
+loss, accuracy, mid1_acc, mid2_acc, mid3_acc = trainer.evaluate(resume_model=args.resume_model,
                                   save_matrix_path=args.save_matrix_path)
 end = time.time()
-print('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.5f}'.format(int(end - start), loss, accuracy))
+print('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.2%}'.format(int(end - start), loss, accuracy))
 
 # 将评估结果写入txt文件 改
-with open('output/MelSpectrogram_results/PANNS_CNN6_results.txt', 'a') as f:
-    f.write('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.5f}\n'.format(int(end - start), loss, accuracy))
+with open('output/Elevator_Mel_results/ResNetSE_eval_results.txt', 'a') as f:
+    f.write('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.2%}, middle1_acc：{:.2%}, middle2_acc：{:.2%}, middle3_acc：{:.2%}\n'.format(int(end - start), loss, accuracy,mid1_acc, mid2_acc, mid3_acc ))
