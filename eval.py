ab54400b410d2fec96c1f8c8b9f0feebee5fd7d9
qiulingyan <kiwiqiuqiu@163.com>
2025-03-24 20:24:53 +0800

    eval.py:新增测试集中分类正确数据的路径,有middle_output

--- eval.py
+++ eval.py
@@ -20,11 +20,11 @@ trainer = MAClsTrainer(configs=args.configs, use_gpu=args.use_gpu)
 
 # 开始评估
 start = time.time()
-loss, accuracy = trainer.evaluate(resume_model=args.resume_model,
+loss, accuracy, mid1_acc, mid2_acc, mid3_acc = trainer.evaluate(resume_model=args.resume_model,
                                   save_matrix_path=args.save_matrix_path)
 end = time.time()
 print('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.2%}'.format(int(end - start), loss, accuracy))
 
 # 将评估结果写入txt文件 改
 with open('output/Elevator_Mel_results/ResNetSE_eval_results.txt', 'a') as f:
-    f.write('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.2%}\n'.format(int(end - start), loss, accuracy ))
+    f.write('评估消耗时间：{}s，loss：{:.5f}，accuracy：{:.2%}, middle1_acc：{:.2%}, middle2_acc：{:.2%}, middle3_acc：{:.2%}\n'.format(int(end - start), loss, accuracy,mid1_acc, mid2_acc, mid3_acc ))
